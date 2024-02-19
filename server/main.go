daa57fa525d0bbbfc5bf4751b4b18b3f63da51ea
Matthew Hope <chickazama@gmail.com>
2024-02-19 18:47:34 +0000

    Implement Client go-routines

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net"
@@ -13,11 +12,12 @@ const (
 )
 
 var (
-	msg chan []byte
+	pool *Pool
 )
 
 func init() {
-	msg = make(chan []byte)
+	pool = NewPool()
+	go pool.Run()
 }
 
 func main() {
@@ -27,33 +27,14 @@ func main() {
 	}
 	defer listener.Close()
 	fmt.Println("Server listening for connections...")
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	go read(conn)
-	write(conn)
-}
-
-func read(conn net.Conn) {
-	defer conn.Close()
-	br := bufio.NewReader(conn)
-	for {
-		br.Reset(conn)
-		buf, err := br.ReadBytes('\n')
+	for i := 0; i < 10; i++ {
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		fmt.Printf("%s", string(buf))
-		msg <- buf
-	}
-}
-
-func write(conn net.Conn) {
-	for m := range msg {
-		n, err := conn.Write(m)
-		if err != nil {
-			log.Fatalf("%s: bytes written: %d", err.Error(), n)
-		}
+		c := NewClient(conn)
+		pool.Clients[i] = c
+		go c.Read()
+		go c.Write()
 	}
 }
