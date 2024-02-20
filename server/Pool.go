91c41cec3e2a51b62565ce345a6accbb71229cc8
Matthew Hope <chickazama@gmail.com>
2024-02-20 23:47:56 +0000

    Redefine connected clients map

--- server/Pool.go
+++ server/Pool.go
@@ -2,41 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
+)
+
+const (
+	queueSize = 8
 )
 
 type Pool struct {
 	In      chan []byte
-	Logout  chan int
-	Clients map[int]*Client
+	Clients map[*Client]bool
 }
 
 func NewPool() *Pool {
 	ret := new(Pool)
-	ret.In = make(chan []byte)
-	ret.Logout = make(chan int)
-	ret.Clients = make(map[int]*Client)
+	ret.In = make(chan []byte, queueSize)
+	ret.Clients = make(map[*Client]bool)
 	return ret
 }
 
 func (p *Pool) Run() {
-	for {
-		select {
-		case m := <-p.In:
-			t := time.Now().UTC().Format(time.DateTime)
-			msg := fmt.Sprintf("[%s] - %s", t, m)
-			for _, c := range p.Clients {
-				c.Out <- []byte(msg)
-			}
-		case id := <-p.Logout:
-			c, exists := p.Clients[id]
-			if exists {
-				c.Conn.Close()
-				delete(p.Clients, id)
-			}
-			for _, c := range p.Clients {
-				c.Out <- []byte("Client exited.\n")
-			}
+	for m := range p.In {
+		t := time.Now().UTC().Format(time.DateTime)
+		msg := fmt.Sprintf("[%s] - %s", t, m)
+		out := []byte(msg)
+		mtx.Lock()
+		_, err := fp.Write([]byte(out))
+		if err != nil {
+			log.Println(err.Error())
+		}
+		mtx.Unlock()
+		out[len(out)-1] = 0
+		for c := range p.Clients {
+			c.Out <- []byte(out)
 		}
 	}
 }
