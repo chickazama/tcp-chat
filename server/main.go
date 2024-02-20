a64e2255f0c4bf72279a60f02ded3b320fbd5e93
Matthew Hope <chickazama@gmail.com>
2024-02-20 23:47:20 +0000

    Write chat history to text file

--- server/main.go
+++ server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"sync"
 )
 
 const (
@@ -11,29 +13,59 @@ const (
 	addr    = "127.0.0.1:49000"
 )
 
+const (
+	maxConnections = 10
+)
+
 var (
-	pool *Pool
+	mtx              sync.Mutex
+	nextId           = 1
+	connectedClients int
+	pool             *Pool
+	fp               *os.File
 )
 
 func init() {
+	var err error
+	fp, err = os.OpenFile("history.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	pool = NewPool()
 	go pool.Run()
 }
 
 func main() {
+	defer fp.Close()
 	listener, err := net.Listen(network, addr)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	defer listener.Close()
 	fmt.Println("Server listening for connections...")
-	for i := 0; i < 10; i++ {
+	for connectedClients < maxConnections {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		c := NewClient(i+1, conn)
-		pool.Clients[i+1] = c
+		c := NewClient(nextId, conn)
+		pool.Clients[c] = true
+		connectedClients++
+		log.Printf("Connected clients: %d\n", connectedClients)
+		mtx.Lock()
+		buf, err := os.ReadFile("history.txt")
+		if err != nil {
+			log.Println(err.Error())
+			buf = []byte("Chat History Unavailable\n")
+		}
+		if len(buf) <= 0 {
+			str := "*** TCP CHAT ***\n"
+			buf = []byte(str)
+			fp.Write(buf)
+		}
+		mtx.Unlock()
+		buf[len(buf)-1] = 0
+		c.Out <- buf
 		go c.Read()
 		go c.Write()
 	}
