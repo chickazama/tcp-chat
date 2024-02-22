1ffad3224f7e9f721a4209355bfbb5f5e0e3f88f
Matthew Hope <chickazama@gmail.com>
2024-02-22 11:22:31 +0000

    Implement Run logic in file

---
+++ client/ui/Run.go
@@ -0,0 +1,44 @@
+package ui
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/awesome-gocui/gocui"
+)
+
+func Run() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	printGreeting()
+	readName()
+	// Set up GUI
+	g, err := gocui.NewGui(gocui.OutputNormal, true)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer g.Close()
+	g.SetManagerFunc(setLayout)
+	err = setKeyBindings(g)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// Spawn a go-routine to update the view
+	go updateOutputView(g)
+	initalOut := fmt.Sprintf("%s\n", name)
+	client.Send <- []byte(initalOut)
+	if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
+		log.Fatal(err.Error())
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
